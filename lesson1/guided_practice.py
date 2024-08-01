42d2a941fb78ec269fac0fb2cf2773c9289a84a8
Herun Tarun <herun.s@gmail.com>
2024-08-01 10:48:09 -0400

    added example to PEDAC guided practice problems

---
+++ lesson1/guided_practice.py
@@ -0,0 +1,15 @@
+# Leftover Blocks
+# Input: An integer that represents the number of available blocks
+# Output: An integer that represents the number of blocks left over 
+#         after building the tallest valid structure
+
+# Explicit:
+# Each block is a cube
+# The structure is built in layers of blocks
+# The top layer is a single block
+# Each block in a layer is supported by four blocks in a lower layer
+# An individual block can support more than one block in an upper layer
+# There are no gaps between blocks
+
+# Implicit:
+# Each block can support a maximum of four blocks
