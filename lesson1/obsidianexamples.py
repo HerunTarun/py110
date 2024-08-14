56ced3060990366b6e5c3333a818dbab7627f0e3
Herun Tarun <herun.s@gmail.com>
2024-08-14 17:16:09 -0400

    added examples for obsidian reorganization

---
+++ lesson1/obsidianexamples.py
@@ -0,0 +1,32 @@
+word = 'dreamcatcher'
+print(len(word))
+# This will print 12
+
+# range
+print(len(range(5)))
+# This will print 5
+
+# tuple
+fruit = ('apple', 'banana', 'cherry')
+print(len(fruit))
+# This will print 3
+
+# list
+games = ['chess', 'checkers', 'go']
+print(len(games))
+# This will print 3
+
+# dictionary
+classical_ratings = {'Magnus': 2832,'Hikaru': 2802,'Fabiano': 2793,'Arjun': 2778}
+print(len(classical_ratings))
+# This will print 4
+
+# set
+vehicles = {'truck', 'sedan', 'carriage'}
+print(len(vehicles))
+# This will print 3
+
+# frozen set
+tactics = frozenset(['pin', 'skewer', 'x-ray'])
+print(len(tactics))
+# This will print 3
