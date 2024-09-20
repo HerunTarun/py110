4df520116d878815a31cdc2a3a369e152815c66f
Herun Tarun <herun.s@gmail.com>
2024-09-20 16:54:23 -0400

    added problem, PEDAC framework, and test cases for palindromic_part_1

---
+++ small_problems/easy1/palindromic_part_1.py
@@ -0,0 +1,39 @@
+# Write a function that returns True if 
+# the string passed as an argument is a palindrome, False otherwise. 
+# A palindrome reads the same forwards and backwards. 
+# For this problem, the case matters and all characters matter.
+
+# PEDAC
+
+# Input
+# String that may or may not be a palindrome
+
+# Output
+# Boolean True or False
+
+# Boundaries/Assumptions
+# Case matters
+# All characters matter
+
+# Examples/Test Cases
+# All of these examples should print True
+
+print(is_palindrome('madam') == True)
+print(is_palindrome('356653') == True)
+print(is_palindrome('356635') == False)
+
+# case matters
+print(is_palindrome('Madam') == False)
+
+# all characters matter
+print(is_palindrome("madam i'm adam") == False)
+
+# Data Structure
+# 
+
+# Algorithm
+# 
+
+# Code
+
+# Refactor
